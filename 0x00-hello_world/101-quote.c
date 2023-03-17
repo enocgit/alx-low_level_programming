706d98f3f4c1ffa3fc5bf3676704597fb22e43a2
Enoch Ansah <enocansah@yahoo.com>
2023-03-17 06:51:09 +0000

    Modify: 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
 int main(void){
   char clause[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
   size_t clause_lenth = sizeof(msg) - 1;
   ssize_t write_to = write(2, clause, clause_length);
 
-  return 1
+  return (1);
 }
