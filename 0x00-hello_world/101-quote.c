85dc4f627a0b377847c0c733202f6a270b9f02dd
Enoch Ansah <enocansah@yahoo.com>
2023-03-17 05:57:41 +0000

    Add: c files and bash files

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void){
+  char clause[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  size_t clause_lenth = sizeof(msg) - 1;
+  ssize_t write_to = write(2, clause, clause_length);
+
+  return 1
+}
