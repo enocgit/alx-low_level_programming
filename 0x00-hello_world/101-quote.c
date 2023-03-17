36edad26e37062a594c6d64d6019c534cc2b6729
Enoch Ansah <enocansah@yahoo.com>
2023-03-17 06:52:12 +0000

    Modify: 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 */
 int main(void){
   char clause[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  size_t clause_lenth = sizeof(msg) - 1;
+  size_t clause_lenth = sizeof(clause) - 1;
   ssize_t write_to = write(2, clause, clause_length);
 
   return (1);
