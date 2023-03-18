d065f315bef2f6ee2bd343f218ac50fc89df51c1
Enoch Ansah <enocansah@yahoo.com>
2023-03-18 15:41:48 +0000

    Modify: 6th c file to match Betty style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-	const char clause[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char clause[] = "and that piece of art is useful\" - "
+		"Dora Korpar, 2015-10-19\n";
 	const size_t clause_len = sizeof(clause) - 1;
 	ssize_t write_to = write(2, clause, clause_len);
 
