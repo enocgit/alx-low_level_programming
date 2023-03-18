d64412399abed5fbb6076e55bcb011acc9651dac
Enoch Ansah <enocansah@yahoo.com>
2023-03-18 15:21:20 +0000

    Modify: 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,16 +4,15 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
-*/
+ */
 int main(void)
 {
-  const char clause[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  const size_t clause_len = sizeof(clause) - 1;
-  ssize_t write_to = write(2, clause, clause_len);
+	const char clause[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const size_t clause_len = sizeof(clause) - 1;
+	ssize_t write_to = write(2, clause, clause_len);
 
-  if (write_to != (ssize_t)clause_len)
-    return (1)
+	if (write_to != (ssize_t)clause_len)
+		return (1)
 
-      return (0);
+	return (0);
 }
-
