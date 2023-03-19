1840426eeac2addabe224431b481863f666166ce
Enoch Ansah <enocansah@yahoo.com>
2023-03-19 06:22:29 +0000

    Modify: 3rd c file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char lowercase;
+	char uppercase;
+
+	lowercase = 'a';
+	uppercase = 'A';
+	while (lowercase <= 'z')
+	{
+		putchar(lowercase);
+		lowercase++;
+	}
+	while (uppercase <= 'Z')
+	{
+		putchar(uppercase);
+		uppercase++;
+	}
+	putchar('\n');
+
+	return 0;
+}
