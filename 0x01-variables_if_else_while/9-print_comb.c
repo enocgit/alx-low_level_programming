a69f358491a1a40b59a0f8a2bb180560777f72af
Enoch Ansah <enocansah@yahoo.com>
2023-03-20 14:34:14 +0000

    Add: 9th c file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int num = '0';
+
+	int count = 0;
+
+	while (count < 10)
+	{
+		putchar(num + count);
+		putchar(',');
+		putchar(' ');
+		count++;
+	}
+	putchar('\n');
+
+	return (0);
+}
