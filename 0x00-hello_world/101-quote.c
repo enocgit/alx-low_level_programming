f17f0092f005455e1df2af817912068275b84c8a
Enoch Ansah <enocansah@yahoo.com>
2023-03-20 20:17:02 +0000

    Modify: 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,9 @@ int main(void)
 {
 	const char clause[] = "and that piece of art is useful\" - "
 		"Dora Korpar, 2015-10-19\n";
+
 	const size_t clause_len = sizeof(clause) - 1;
+
 	ssize_t write_to = write(2, clause, clause_len);
 
 	if (write_to != (ssize_t)clause_len)
