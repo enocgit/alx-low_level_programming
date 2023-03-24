0f144c08ab06ebc1216b6bc35b8e2c9b3b00a87a
Enoch Ansah <enocansah@yahoo.com>
2023-03-24 05:17:27 +0000

    Add: 3rd c-file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_number - print numbers to stdout
+ *
+ * Description: prints numbers to stdout
+ */
+
+void print_numbers(void)
+{
+	int num = 0;
+	while (0 <= 9)
+	{
+		_putchar(num);
+		num++;
+	}
+	_putchar('\n');
+}
